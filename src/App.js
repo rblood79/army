16f0b9472bc95f191dfe39f9b827b24bbfbaf71c
rblood79 <rblood79@gmail.com>
2021-10-23 01:07:35 +0900

    new

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = (props) => {
     //console.log(props.location.pathname)
   }, [])
   return (
-    <div className={isMobile ? "App mobile" : "App"} style={{backgroundImage: `url(ptn.png)`, height: props.location.pathname === '/' && '100%'}}>
+    <div className={isMobile ? "App mobile" : "App"} style={{height: props.location.pathname === '/' && '100%'}}>
       {props.location.pathname === '/' ?  <div className='headM'><img src='./mnd_logo_signature.png' alt='국방부'/></div> : props.location.pathname === '/result' &&  <Head />}
       <main className='main' style={{flex: props.location.pathname === '/result' && 1}}>
         <Route exact path="/" render={() => <Home user={usersRef} mobile={isMobile}/>} />
--- src/page/Home.js
+++ src/page/Home.js
@@ -65,9 +65,9 @@ const App = (props) => {
     <div className='container'>
       {
         (!data) ?
-          <div className='login' style={{ backgroundImage: "url('sorder0.png')" }}>
-            <h2><span className='subTitle'>화생방 보호의 신청{!props.mobile && ' 관리'}</span></h2>
-            <div>
+          <div className='login'>
+            <h2 style={{paddingBottom: type && '175px'}}><span className='subTitle'>화생방 보호의 신청{!props.mobile && ' 관리'}</span></h2>
+            <div className='armyWrap'>
               {
                 props.mobile && !type ?
                 <>
@@ -141,13 +141,13 @@ const App = (props) => {
             :
             <div className='order'>
               <div>
-                <h2 className='title'>
+                {/*<h2 className='title'>
                   <div className='profile'><i className="ri-shield-user-line"></i> 안녕하세요 {type}소속 {number}님</div>
-                </h2>
+              </h2>*/}
                 <div className='accordion'>
                   <div className='accordionItem'>
                     <h3 className='accordionHead' onClick={() => { active === 0 ? setActive(null) : setActive(0) }}>보호의{armor ? ' "' + armor + '" 를 선택하였습니다' : '를 선택하세요'}</h3>
-                    <div className={active === 0 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 0 && 42 * 7 }}>
+                    <div className={active === 0 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 0 && 48 * 7 }}>
                       <input type='radio' name='armor' id='armor0' value='특1호' onChange={({ target: { value } }) => setArmor(value)} /><label htmlFor='armor0' className='label'><span>특1호</span><span>신장 191cm이상</span></label>
                       <input type='radio' name='armor' id='armor1' value='1호' onChange={({ target: { value } }) => setArmor(value)} /><label htmlFor='armor1' className='label'><span>1호</span><span>신장 185cm - 190cm</span></label>
                       <input type='radio' name='armor' id='armor2' value='2호' onChange={({ target: { value } }) => setArmor(value)} /><label htmlFor='armor2' className='label'><span>2호</span><span>신장 180cm - 184cm</span></label>
@@ -159,14 +159,14 @@ const App = (props) => {
                   </div>
                   <div className='accordionItem'>
                     <h3 className='accordionHead' onClick={() => { active === 1 ? setActive(null) : setActive(1) }}>덧신{shoes ? ' "' + shoes + '" 를 선택하였습니다' : '을 선택하세요'}</h3>
-                    <div className={active === 1 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 1 && 42 * 2 }}>
+                    <div className={active === 1 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 1 && 48 * 2 }}>
                       <input type='radio' name='shoes' id='shoes0' value='대' onChange={({ target: { value } }) => setShoes(value)} /><label htmlFor='shoes0' className='label'><span>대</span><span>발 270mm이상</span></label>
                       <input type='radio' name='shoes' id='shoes2' value='소' onChange={({ target: { value } }) => setShoes(value)} /><label htmlFor='shoes2' className='label'><span>소</span><span>발 270mm미만</span></label>
                     </div>
                   </div>
                   <div className='accordionItem'>
                     <h3 className='accordionHead' onClick={() => { active === 2 ? setActive(null) : setActive(2) }}>장갑{gloves ? ' "' + gloves + '" 를 선택하였습니다' : '을 선택하세요'}</h3>
-                    <div className={active === 2 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 2 && 42 * 3 }}>
+                    <div className={active === 2 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 2 && 48 * 3 }}>
                       <input type='radio' name='gloves' id='gloves0' value='대' onChange={({ target: { value } }) => setGloves(value)} /><label htmlFor='gloves0' className='label'><span>대</span><span>길이 23cm이상</span></label>
                       <input type='radio' name='gloves' id='gloves1' value='중' onChange={({ target: { value } }) => setGloves(value)} /><label htmlFor='gloves1' className='label'><span>중</span><span>길이 20 - 22cm</span></label>
                       <input type='radio' name='gloves' id='gloves2' value='소' onChange={({ target: { value } }) => setGloves(value)} /><label htmlFor='gloves2' className='label'><span>소</span><span>길이 19cm이하</span></label>
@@ -174,7 +174,7 @@ const App = (props) => {
                   </div>
                   <div className='accordionItem'>
                     <h3 className='accordionHead' onClick={() => { active === 3 ? setActive(null) : setActive(3) }}>방독면{mask ? ' "' + mask + '" 를 선택하였습니다' : '을 선택하세요'}</h3>
-                    <div className={active === 3 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 3 && 42 * 4 }}>
+                    <div className={active === 3 ? 'accordionBodyActive' : 'accordionBody'} style={{ height: active === 3 && 48 * 4 }}>
                       <input type='radio' name='mask' id='mask0' value='특대' onChange={({ target: { value } }) => setMask(value)} /><label htmlFor='mask0' className='label'><span>특대</span><span>XL</span></label>
                       <input type='radio' name='mask' id='mask1' value='대' onChange={({ target: { value } }) => setMask(value)} /><label htmlFor='mask1' className='label'><span>대</span><span>L</span></label>
                       <input type='radio' name='mask' id='mask2' value='중' onChange={({ target: { value } }) => setMask(value)} /><label htmlFor='mask2' className='label'><span>중</span><span>M</span></label>
@@ -184,7 +184,7 @@ const App = (props) => {
                 </div>
               </div>
               <div className='controll'>
-                <p>{armor && shoes && gloves && mask ? '선택하신 사이즈로 신청하시겠습니까' : '본인의 사이즈에 맞게 선택해주세요'}</p>
+                <p>{armor && shoes && gloves && mask ? '선택하신 사이즈로 신청하시겠습니까' : number+'님 사이즈에 맞게 선택해주세요'}</p>
                 <button className={'button'} disabled={!armor || !shoes || !gloves || !mask} onClick={() => {
                   onSave()
                 }}>신청</button>
